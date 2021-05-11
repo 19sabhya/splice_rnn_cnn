77e983ef159145218b806db75edaa94a65c9858e
Sabhya A. Lokhande <64120740+19sabhya@users.noreply.github.com>
2021-05-11 16:21:53 +0530

    Update create_dataset_csv_file.py

--- create_dataset_csv_file.py
+++ create_dataset_csv_file.py
@@ -6,10 +6,10 @@ import random
 
 def create_dataset_file(myDir, is_tp_files):
 	if is_tp_files:
- 		data_csv_file = open("../data/tpImages.csv", "w")
+ 		data_csv_file = open("../splice_rnn_cnn/data/spImages.csv", "w")
 	else:
-		data_csv_file = open("../data/auImages.csv", "w")
-	format_ = ['.JPG' , '.jpg', 'jpeg', '.png', '.tiff', '.TIFF', '.Tiff', '.Tif', '.TIF', '.tif']
+		data_csv_file = open("../splice_rnn_cnn/data/auImages.csv", "w")
+	format_ = ['.JPG' , '.jpg', 'jpeg', '.png', '.tiff', '.TIFF', '.Tiff', '.Tif', '.TIF', '.tif', '.bmp']
 	print(myDir)
 	for root, dirs, files in os.walk(myDir, topdown=False):
 		for name in files:
@@ -23,6 +23,6 @@ def create_dataset_file(myDir, is_tp_files):
 
 
 # load the original image
-create_dataset_file('../data/CASIA2/Au', 0)
-create_dataset_file('../data/CASIA2/Tp', 1)
+create_dataset_file('../splice_rnn_cnn/data/Au', 0)
+create_dataset_file('../splice_rnn_cnn/data/Sp', 1)
 
